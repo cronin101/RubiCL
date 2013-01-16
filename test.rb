e1962baa635b0d3c37e9aebbc65413282e069210
Aaron Cronin <aaron@freeagent.com>
2013-01-16 16:08:08 +0000

    Removed stale testing blurb

--- test.rb
+++ test.rb
@@ -1,5 +1,6 @@
 require './hadope.rb'
 
+# Creating a DataSet
 HaDope::DataSet.create({
   name: :one_to_onehundred,
   type: :int,
@@ -27,30 +28,5 @@ HaDope::Filter.create({
 kernel
 })
 
-#Showing names of DataSets
-all_data_sets = HaDope::DataSet.names
-puts all_data_sets.inspect
-
-#Showing names of Maps
-all_map_names = HaDope::Map.names
-puts all_map_names.inspect
-
-#Showing names of Filters
-all_filter_names = HaDope::Filter.names
-puts all_filter_names.inspect
-
-#Retrieving a single DataSet by name
-a_data_set = HaDope::DataSet[:one_to_onehundred]
-puts a_data_set.inspect
-
-#Retrieving a single Map function by name
-a_map = HaDope::Map[:add_one]
-puts a_map.inspect
-
-#Retrieving a single Filter function by name
-a_filter = HaDope::Filter[:doubled_is_even?]
-puts a_filter.inspect
-
 #Idea of how to execute a series of actions
 results = HaDope::GPU.new.load(:one_to_onehundred).map(:add_one).filter(:doubled_is_even?).output
-puts results.inspect
