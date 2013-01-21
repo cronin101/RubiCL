8e5836019539d7a7b61fdd88e6bc4aa9b7f941da
Aaron Cronin <cronin@live.co.uk>
2013-01-21 21:45:00 +0000

    Added error header to makefile

--- extensions/extconf.rb
+++ extensions/extconf.rb
@@ -4,5 +4,6 @@ require 'mkmf'
 extension_name = 'hadope_backend'
 dir_config(extension_name)
 have_header('hadope.h')
+have_header('oclerrorexplain.h')
 have_library('OpenCL')
 create_makefile(extension_name)
