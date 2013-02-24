38d853f1146a51c10b22db55b7fce5bdfab80fc1
Aaron Cronin <cronin@live.co.uk>
2013-02-24 22:21:59 +0000

    No more ugly curly braces for benchmarking and output caching until destructive commands

--- lib/devices/opencl_device.rb
+++ lib/devices/opencl_device.rb
@@ -2,9 +2,20 @@ class HaDope
   module DeviceMethods
 
     def load(dataset_name)
+      clear_cache
+
       dataset = HaDope::DataSet[dataset_name]
-      puts "Buffer Time: #{Benchmark.realtime{ @membuffer = create_memory_buffer(dataset.data[:size], dataset.type.to_s) }}"
-      puts "Dataset Input Time: #{Benchmark.realtime{ load_int_dataset(dataset.data[:values], @membuffer) }}"
+
+      buffer_time = Benchmark.realtime do
+        @membuffer = create_memory_buffer(dataset.data[:size], dataset.type.to_s)
+      end
+      puts "Buffer Time: #{buffer_time}"
+
+      input_time = Benchmark.realtime do
+        load_int_dataset(dataset.data[:values], @membuffer)
+      end
+      puts "Dataset Input Time: #{input_time}"
+
       self
     end
 
@@ -19,13 +30,21 @@ class HaDope
     end
 
     def presence_array
-      retrieve_int_dataset(@presence_array)
+      output_time = Benchmark.realtime do
+        @presence_array ||= retrieve_int_dataset(@presence_buffer)
+      end
+      puts "Dataset Output Time: #{output_time}"
+
+      @presence_array
     end
 
     def output
-      dataset = []
-      puts "Dataset Output Time: #{Benchmark.realtime{ dataset = retrieve_int_dataset(@membuffer) }}"
-      dataset
+      output_time = Benchmark.realtime do
+        @output ||= retrieve_int_dataset(@membuffer)
+      end
+      puts "Dataset Output Time: #{output_time}"
+
+      @output
     end
 
     def clean
@@ -33,17 +52,32 @@ class HaDope
     end
 
     private
+    def clear_cache
+      @output, @presence_array = [nil]*2
+    end
 
     def do_fp_map(task_name)
+      clear_cache
+
       map_task = HaDope::Functional::Map[task_name]
       kernel = map_task.kernel
-      puts "#{task_name} Time: #{Benchmark.realtime{ run_map_task(kernel, kernel.length, map_task.name.to_s, @membuffer) }}"
+
+      map_time = Benchmark.realtime do
+        run_map_task(kernel, kernel.length, map_task.name.to_s, @membuffer)
+      end
+      puts "#{task_name} Time: #{map_time}"
     end
 
     def do_fp_filter(task_name)
+      clear_cache
+
       filter_task = HaDope::Functional::Filter[task_name]
       kernel = filter_task.kernel
-      puts "#{task_name} Time: #{Benchmark.realtime{ @presence_array = run_filter_task(kernel, kernel.length, filter_task.name.to_s, @membuffer) }}"
+
+      filter_time = Benchmark.realtime do
+        @presence_buffer = run_filter_task(kernel, kernel.length, filter_task.name.to_s, @membuffer)
+      end
+      puts "#{task_name} Time: #{filter_time}"
     end
 
   end
