33e0eccb6d4012a28977881ce19d2417905fb0ce
Aaron Cronin <cronin@include.cat>
2014-02-09 22:39:09 +0000

    Neaten monkey-patching

--- lib/types/array.rb
+++ lib/types/array.rb
@@ -1,13 +1,13 @@
 class Array
-  alias_method :__old_array_index_access, :[]
-  def [](index)
+  old_behavior = instance_method(:[])
+  define_method :[] do |index|
     case index
     when Class
       raise 'Must specify OpenCL device with Hadope.set_device' unless Hadope.opencl_device
       raise "#{index.inspect} is not defined as convertible." unless index.respond_to? :hadope_conversion
       Hadope.opencl_device.send(index.hadope_conversion, self)
     else
-      __old_array_index_access(index)
+      old_behavior.bind(self).(index)
     end
   end
 end
