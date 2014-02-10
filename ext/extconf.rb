11c58ec584f26eb985c13d8faa3b3dc78232bafd
Aaron Cronin <cronin@include.cat>
2014-02-10 12:36:27 +0000

    2Portable4U

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,16 +1,22 @@
 #!/usr/bin/env ruby
 require 'mkmf'
 
-$LIBS << ' -framework OpenCL'
 $objs = %w{lib/oclerrorexplain.o lib/prefix_sum/prescan.o lib/hadope.o hadope_backend.o}
 
 extension_name = 'hadope_backend'
 
-dir_config(extension_name)
+dir_config extension_name
 
-have_header('lib/hadope.h')
-have_header('lib/oclerrorexplain.h')
-have_header('lib/prefix_sum/prescan.h')
+have_header 'lib/hadope.h'
+have_header 'lib/oclerrorexplain.h'
+have_header 'lib/prefix_sum/prescan.h'
+
+case Gem::Platform.local.os
+when 'darwin'
+  $LIBS << ' -framework OpenCL'
+else
+  have_library 'OpenCL'
+end
 
 create_header
-create_makefile(extension_name)
+create_makefile extension_name
