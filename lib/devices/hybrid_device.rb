8ac22815b19bcc6e7af1cc25202e43a0b6e59f76
Aaron Cronin <cronin@include.cat>
2014-02-23 18:11:54 +0000

    Benchmark on hybrid device initialization

--- lib/devices/hybrid_device.rb
+++ lib/devices/hybrid_device.rb
@@ -23,13 +23,58 @@ module Hadope
 
     def initialize
       @ratio = {}
-      #BENCHMARKS.each { |task, test| @ratio[task] = send test }
+      BENCHMARKS.each { |task, test| @ratio[task] = send test }
       @environment = initialize_hybrid_environment
       super
     end
 
     def [](type)
       send type.hadope_conversion
+    end
+
+    private
+
+    TEST_ARRAY_LENGTH = 50
+    TEST_DATASET_1    = (1..TEST_ARRAY_LENGTH).to_a
+
+    def map_test_benchmark
+      benchmark_action do |device|
+        device.pin_integer_dataset(TEST_DATASET_1)
+          .map { |x| x * x }[Fixnum]
+      end
+    end
+
+    def filter_task_benchmark
+      benchmark_action do |device|
+        device.pin_integer_dataset(TEST_DATASET_1)
+          .filter { |x| x % 2 == 0 }[Fixnum]
+      end
+    end
+
+    def mapfilter_task_benchmark
+      benchmark_action do |device|
+        device.pin_integer_dataset(TEST_DATASET_1)
+          .map { |x| x + 1 }
+          .filter { |x| x % 2 == 0 }[Fixnum]
+      end
+    end
+
+    def individual_devices
+      return Hadope::CPU.get, Hadope::GPU.get
+    end
+
+    def benchmark_action(&block)
+      cpu, gpu = individual_devices
+
+      cpu_time, gpu_time = [cpu, gpu].map do |d|
+        Benchmark.realtime do
+          block.call(d)
+        end
+      end
+
+      puts "CPU: #{cpu_time}, GPU: #{gpu_time}"
+      puts "CPU_RATIO: #{ratio = Rational(cpu_time, gpu_time).truncate(+1)}"
+      ratio
     end
 
   end
