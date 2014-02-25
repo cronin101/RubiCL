a822b67fa3fa0d091d7d969f9564c7dbcec7c93f
Aaron Cronin <cronin@include.cat>
2014-02-25 16:58:01 +0000

    Update timings script

--- benchmarks/sort_benchmark.rb
+++ benchmarks/sort_benchmark.rb
@@ -1,19 +1,27 @@
 require 'asymptotic'
 require './hadope'
 
-seeds = (10..24)
+seeds = (10..22)
 
 ruby_input = {
   input_seeds: seeds,
   input_function: ->(pow){ (1..2**pow).to_a.shuffle }
 }
 Asymptotic::Graph.plot(3, "Sorting Integers",
-  "RubiCL library [CPU: Intel i7 dual-core (MBA)]" => {
-    function: ->(array){ Hadope.opencl_device = Hadope::CPU; array[Int].sort[Fixnum] }
+  "RubiCL library [CPU: Intel i7 dual-core (MBA)] host_mem = t" => {
+    function: ->(array){ Hadope::Config::Features.use_host_mem = true; Hadope.opencl_device = Hadope::CPU; array[Int].sort[Fixnum] }
   }.merge(ruby_input),
 
-  "RubiCL library [GPU: Intel HD5000 (MBA)]" => {
-    function: ->(array){ Hadope.opencl_device = Hadope::GPU; array[Int].sort[Fixnum] }
+  "RubiCL library [CPU: Intel i7 dual-core (MBA)] host_mem = f" => {
+    function: ->(array){ Hadope::Config::Features.use_host_mem = false; Hadope.opencl_device = Hadope::CPU; array[Int].sort[Fixnum] }
+  }.merge(ruby_input),
+
+  "RubiCL library [GPU: Intel HD5000 (MBA)] host_mem = t" => {
+    function: ->(array){ Hadope::Config::Features.use_host_mem = true; Hadope.opencl_device = Hadope::GPU; array[Int].sort[Fixnum] }
+  }.merge(ruby_input),
+
+  "RubiCL library [GPU: Intel HD5000 (MBA)] host_mem = f" => {
+    function: ->(array){ Hadope::Config::Features.use_host_mem = false; Hadope.opencl_device = Hadope::GPU; array[Int].sort[Fixnum] }
   }.merge(ruby_input),
 
 
--- get_timings.rb
+++ get_timings.rb
@@ -1,31 +1,31 @@
 require 'benchmark'
 require './hadope'
 
-sizes = (500_000..8_000_000).step(500_000)
+sizes = (1_000_000..15_000_000).step(2_000_000)
 
 # Ruby timings
 file = File.open('./timings', 'w')
 
 sizes.each do |size|
-  time = (1..5).map do
-    sleep 0.2
+  time = (1..8).map do
+    sleep 0.1
     Benchmark.realtime do
       (1..size).map { |x| x * x }.select { |x| x % 2 == 0 }
     end
-  end.inject(&:+) / 5.0
-  file.puts "vanilla_ruby #{size} #{time}"
+  end.inject(&:+) / 8.0
+  file.puts "vanilla_ruby #{size} #{time.round(3)}"
 end
 
 [[Hadope::CPU, 'rubicl_cpu'], [Hadope::GPU, 'rubicl_gpu']].each do |device, description|
   Hadope.opencl_device = device
   (1..100)[Int].map { |x| x + 1 }[Fixnum]
   sizes.each do |size|
-    time = (1..5).map do
+    time = (1..8).map do
       sleep 0.2
       Benchmark.realtime do
         (1..size)[Int].map { |x| x * x }.select { |x| x % 2 == 0 }[Fixnum]
       end
-    end.inject(&:+) / 5.0
+    end.inject(&:+) / 8.0
     file.puts "#{description} #{size} #{time.round(3)}"
   end
 end
